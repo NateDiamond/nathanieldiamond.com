3f9dfe0afd3c59991492d68e329d473a58317239
Nathaniel Diamond <nathanieldiamond@Nathaniels-MacBook-Pro.local>
2018-07-22 23:42:04 -0700

    Functionality for different pages on SideNav using different React components. May have to refactor later if there is need for more commonality, though the App component should take care of the majority of the intersection. Just may have more inheritance lower down. Also, note moving forward about the color scheme. A helper function will have to be created that copies the style aspect of the state and then alters the color. Then, any other CSS changes can be done with Materialize classes and with App.css. Inline coloring will thus look like style = {this.state.primary}, or style = {primary} if that was defined globally. Defining on this style attribute is effectively the same work as specifying the className, so I think this is a fine compromise.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {page: HomePage}
+  }
   render() {
     return (
       <div className="App">
+        <SideNav onClick = {(page)=> this.handleClick(page)}/>
+        {React.createElement(this.state.page)}
+      </div>
+    );
+  }
+
+  handleClick(page) {
+    this.setState({page: page});
+  }
+}
+
+class SideNav extends Component {
+  render() {
+    return (
+      <div className="SideNav">
+        <ul id="slide-out" class="sidenav left-align">
+          <li><a href="#!">Home</a></li>
+          <li><a href="#!">Second Sidebar Link</a></li>
+          <li class="no-padding">
+            <ul class="collapsible collapsible-accordion">
+              <li>
+                <a class="collapsible-header">Dropdown<i class="material-icons">add</i></a>
+                <div class="collapsible-body">
+                  <ul>
+                    <li><a onClick= {(page) => this.props.onClick(ColorPage)}>First</a></li>
+                    <li><a href="#!">Second</a></li>
+                    <li><a href="#!">Third</a></li>
+                    <li><a href="#!">Fourth</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <a href="#" data-target="slide-out" class="sidenav-trigger left"><i class="material-icons">menu</i></a>
+      </div>
+    );
+  }
+}
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="HomePage">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,6 +61,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      </div>
+    );
+  }
+}
+
+class ColorPage extends Component {
+  render() {
+    return (
+      <div className="ColorPage">
+        <p>Color</p>
       </div>
     );
   }
