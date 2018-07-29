8559fe37188bfc59d4dc76143f5945a3fe4a97ee
Nathaniel Diamond <nathanieldiamond@Nathaniels-MBP.attlocal.net>
2018-07-28 23:22:53 -0500

    Generalized Title to All Pages and Fixed SideNav

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SideNav onClick = {(page)=> this.handleClick(page)}/>
+        <SideNav onClick = {(page)=> this.changePage(page)}/>
         {React.createElement(this.state.page)}
       </div>
     );
   }
 
-  handleClick(page) {
+  changePage(page) {
     this.setState({page: page});
   }
 }
@@ -25,23 +25,49 @@ class SideNav extends Component {
   render() {
     return (
       <div className="SideNav">
-        <ul id="slide-out" class="sidenav left-align">
-          <li><a href="#!" onClick= {(page) => this.props.onClick(HomePage)}>Home</a></li>
-          <li><a href="#!">Second Sidebar Link</a></li>
-          <li class="no-padding">
+        <ul id="slide-out" class="sidenav">
+          <li class = "no-padding">
+           <ul class="collapsible collapsible-accordion">
+            <li>
+              <a class="collapsible-header" onClick= {(page) => this.props.onClick(HomePage)}>Home</a>
+              <div class="collapsible-body">
+                <ul>
+                  <li><a href="#!">Me</a></li>
+                  <li><a href="#!">Education</a></li>
+                  <li><a href="#!">Work Experience</a></li>
+                </ul>
+              </div>
+            </li>
+            </ul>
+          </li>
+          <li>
+            <a class="collapsible-header" onClick= {(page) => this.props.onClick(Blog)}>Blog</a>
+          </li>
+          <li>
+            <a class="collapsible-header" onClick= {(page) => this.props.onClick(Music)}>Music</a>
+          </li>
+          <li>
+            <a class="collapsible-header" onClick= {(page) => this.props.onClick(Running)}>Running</a>
+          </li>
+          <li>
+            <a class="collapsible-header" onClick= {(page) => this.props.onClick(Games)}>Games</a>
+          </li>
+          <li>
             <ul class="collapsible collapsible-accordion">
               <li>
-                <a class="collapsible-header">Dropdown<i class="material-icons right">add</i></a>
+                <a class="collapsible-header" onClick= {(page) => this.props.onClick(Settings)}>Settings</a>
                 <div class="collapsible-body">
                   <ul>
-                    <li><a onClick= {(page) => this.props.onClick(ColorPage)}>First</a></li>
-                    <li><a href="#!">Second</a></li>
-                    <li><a href="#!">Third</a></li>
-                    <li><a href="#!">Fourth</a></li>
+                    <li><a href="#!">Color</a></li>
+                    <li><a href="#!">Font</a></li>
+                    <li><a href="#!">Animation</a></li>
                   </ul>
                 </div>
               </li>
             </ul>
+          </li>
+          <li>
+            <a class="collapsible-header" onClick= {(page) => this.props.onClick(ContactMe)}>Contact Me</a>
           </li>
         </ul>
         <a href="#" data-target="slide-out" class="sidenav-trigger left"><i class="material-icons medium orange-text">keyboard_arrow_right</i></a>
@@ -52,53 +78,117 @@ class SideNav extends Component {
 
 class HomePage extends Component {
   render() {
-    const speedBetweenLetters = 50;
-    const animationDuration = 1000; //really specified on the HTML elements
-    const offset = 400;
-    var delay = Array(17).fill(null);
-    for(var i = 0; i < 16; i++) {
-      delay[i] = {animationDelay: speedBetweenLetters*i + "ms"};
-    }
-    delay[16] = {animationDelay: (speedBetweenLetters*16 + animationDuration + offset) + "ms"}
     return (
-      <div className="HomePage">
-        <div class="row">
-          <header className="App-header">
-            <div class = "App-title-container animated tada valign-wrapper hide-on-small-only" style = {delay[16]}>
-              <div class="App-title-sub-container">
-                <h1 class="App-title animated bounceInDown" style = {delay[0]} >N</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[1]} >a</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[2]} >t</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[3]} >h</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[4]} >a</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[5]} >n</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[6]} >i</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[7]} >e</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[8]} >l</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[9]} >&nbsp;D</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[10]} >i</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[11]} >a</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[12]} >m</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[13]} >o</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[14]} >n</h1>
-                <h1 class="App-title animated bounceInDown" style = {delay[15]} >d</h1>
-              </div>
-            </div>
-          </header>
-        </div>
-      </div>
+      <Title title = "Nathaniel Diamond"/>
     );
   }
 }
 
-class ColorPage extends Component {
+class Blog extends Component {
   render() {
     return (
-      <div className="ColorPage">
-        <p>Color</p>
-      </div>
+      <Title title = "Blog" />
     );
   }
+}
+
+class Music extends Component {
+  render() {
+    return (
+      <Title title = "Music" />
+    );
+  }
+}
+
+class Running extends Component {
+  render() {
+    return (
+      <Title title = "Running" />
+    );
+  }
+}
+
+class Games extends Component {
+  render() {
+    return (
+      <Title title = "Games" />
+    );
+  }
+}
+
+class Settings extends Component {
+  render() {
+    return (
+      <Title title = "Settings" />
+    );
+  }
+}
+
+class ContactMe extends Component {
+  render() {
+    return (
+      <Title title = "Contact Me" />
+    );
+  }
+}
+
+class Title extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title
+    };
+  }
+  render() {
+    return (
+    <div className="HomePage">
+      <div class="row">
+        <header className="App-header">
+          <div class = "App-title-container animated valign-wrapper hide-on-small-only">
+            <div class = "App-title-sub-container">
+              {bounceDownTitle(this.state.title)}
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+    );
+  }
+}
+
+class AnimatedLetter extends Component {
+  constructor(props) {
+    super(props);
+    var letter;
+    if(this.props.letter === " ") {
+      letter = "\u00a0";
+    } else {
+      letter = this.props.letter;
+    }
+    this.state = { animation: this.props.animation,
+      delay: {animationDelay: this.props.delay + "ms"},
+      letter: letter,
+      className: this.props.className
+    };
+  }
+  render() {
+      const delay = {animationDelay: this.state.delay};
+      const x = this.state.className + " animated " + this.state.animation;
+      return(<h1 class= {x} style = {this.state.delay} >{this.state.letter}</h1>);
+  }
+}
+
+function renderAnimatedLetter(animation, delay, letter, className) {
+  return <AnimatedLetter className = {className} animation = {animation} delay = {delay} letter = {letter} />
+}
+
+function bounceDownTitle(page) {
+  const speedBetweenLetters = 50;
+  var elems = [];
+  for (var i = 0; i < page.length; i ++) {
+    elems.push(renderAnimatedLetter("bounceInDown", speedBetweenLetters*i, page.substr(i,1), "App-title"));
+  }
+  return elems;
 }
 
 export default App;
